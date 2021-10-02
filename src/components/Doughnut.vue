ac4ffdd764a81fa5fef66f4e18108e23acd08414
psy <me@pys.im>
2021-10-02 21:02:39 +0800

    feat: chartjs demo

---
+++ src/components/Doughnut.vue
@@ -0,0 +1,80 @@
+<template>
+    <div style="width: 500px;height: 500px;">
+        <canvas id="myChart" width="200" height="200"></canvas>
+    </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+    mounted() {
+
+        let myChartElement = document.getElementById('myChart');
+
+        let backendData = this.fetchData(); // from backend
+
+        //     [
+        //         {
+        //             label: 'Label1',
+        //             price: 300,
+        //         },
+        //         {
+        //             label: 'Label2',
+        //             price: 50,
+        //         },
+        //         {
+        //             label: 'Label3',
+        //             price: 100,
+        //         },
+        //     ];
+
+        // pluck label value
+        let myLabels = backendData.map(item => item.label);
+
+        // pluck price value
+        let myPrices = backendData.map(item => item.price);
+
+        let myBackgroundColors = [];
+
+        for (let i = 0; i < backendData.length; i++) {
+            myBackgroundColors[i] = `rgb(${i}, ${i + 99}, ${i + 120})`;
+        }
+
+        let myOption = {
+            type: 'doughnut',
+            data: {
+                labels: myLabels, // replace 1
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: myPrices, // replace 2
+                    backgroundColor: myBackgroundColors, // replace 3
+                    hoverOffset: 4
+                }]
+            },
+        };
+
+        let chartInstance = new Chart(myChartElement, myOption);
+
+    },
+    methods: {
+        fetchData() {
+            // request from api
+            return [
+                {
+                    label: 'Label1',
+                    price: 300,
+                },
+                {
+                    label: 'Label2',
+                    price: 50,
+                },
+                {
+                    label: 'Label3',
+                    price: 100,
+                },
+            ];
+        }
+    },
+}
+</script>
---
+++ src/components/Line.vue
@@ -0,0 +1,41 @@
+<template>
+    <div style="width: 500px;height: 500px;">
+        <canvas id="myChartLine" width="200" height="200"></canvas>
+    </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+    mounted() {
+
+        let myChartElement = document.getElementById('myChartLine');
+
+        let backendData = this.fetchData(); // from backend
+
+        let myOption = {
+            type: 'line',
+            data: {
+                labels: [1, 2, 3, 4, 5, 6, 7],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: backendData,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+        };
+
+        new Chart(myChartElement, myOption);
+
+    },
+    methods: {
+        fetchData() {
+            // request from api
+            return [65, 59, 80, 81, 56, 55, 40];
+        }
+    },
+}
+</script>
