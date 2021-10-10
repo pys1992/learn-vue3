66a27f9ac6d9b39a88dc74eb4a8825eebc666310
psy <me@pys.im>
2021-10-10 09:58:56 +0800

    sidebar layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-    <div id="charts">
-        <Doughnut></Doughnut>
-        <Line></Line>
-        <Count></Count>
+    <div style="display: flex;">
+        <div style="width: 200px;background-color: red">
+            <Sidebar></Sidebar>
+        </div>
+        <div id="charts">
+            <Doughnut></Doughnut>
+            <Line></Line>
+            <Count></Count>
+        </div>
     </div>
 </template>
 
@@ -10,12 +15,14 @@
 import Doughnut from './components/Doughnut.vue'
 import Line from './components/Line.vue'
 import Count from './components/ProductCountBarChart.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
     components: {
         Doughnut,
         Line,
-        Count
+        Count,
+        Sidebar
     },
 }
 
